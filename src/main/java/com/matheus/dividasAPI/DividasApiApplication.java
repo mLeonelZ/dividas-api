1a7b1873f0f765e9e60438b3a51c3832c0e27774
mLeonelZ <matheus.leoonel@gmail.com>
2026-09-12 13:40:47 -0300

    chore: initial project setup

---
+++ src/main/java/com/matheus/dividasAPI/DividasApiApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.dividasAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DividasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/dividasAPI/DividasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.dividasAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
